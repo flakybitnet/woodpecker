9329559ea4da1a17fa65fad2588d3425ab65429a
Fernando Barbosa <fernando.cardoso@quintoandar.com.br>
2024-09-13 11:20:39 -0300

    Increase verbosity of logs when failing to create pipeline (4107)

--- server/pipeline/create.go
+++ server/pipeline/create.go
@@ -87,7 +87,7 @@ func Create(ctx context.Context, _store store.Store, repo *model.Repo, pipeline 
 
 		return nil, ErrFiltered
 	} else if configFetchErr != nil {
-		log.Debug().Str("repo", repo.FullName).Err(configFetchErr).Msgf("error while fetching config '%s' in '%s' with user: '%s'", repo.Config, pipeline.Ref, repoUser.Login)
+		log.Error().Str("repo", repo.FullName).Err(configFetchErr).Msgf("error while fetching config '%s' in '%s' with user: '%s'", repo.Config, pipeline.Ref, repoUser.Login)
 		return nil, updatePipelineWithErr(ctx, _forge, _store, pipeline, repo, repoUser, fmt.Errorf("pipeline definition not found in %s", repo.FullName))
 	}
 
