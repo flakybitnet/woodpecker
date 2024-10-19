bf7536df414efd362ecf4649884f260ae85f43fe
0x1def <0x1def@flakybit.net>
2024-10-20 02:24:37 +0300

    CI: fixed ECR publishing & ignoring failing publish steps

--- .ci/publish-external.sh
+++ .ci/publish-external.sh
@@ -7,22 +7,26 @@ set +a
 
 src_image="$HARBOR_REGISTRY/$APP_NAME/$APP_COMPONENT:$APP_VERSION"
 dst_image="$DEST_REGISTRY/$EXTERNAL_REGISTRY_NAMESPACE/$APP_NAME-$APP_COMPONENT:$APP_VERSION"
+dst_authfile="$HOME/auth.json"
 
+echo && echo "Setting authentication for $DEST_REGISTRY"
 if printf '%s' "$DEST_REGISTRY" | grep -q "ecr.aws"; then
+  setRegistryCredHelper "$dst_authfile" "$DEST_REGISTRY" 'ecr-login'
   dst_image="$DEST_REGISTRY/$EXTERNAL_REGISTRY_NAMESPACE/$APP_NAME/$APP_COMPONENT:$APP_VERSION"
-  DEST_CREDS=$(cat "$AWS_CREDS_FILE")
+else
+  setRegistryAuth "$dst_authfile" "$DEST_REGISTRY" "$DEST_CREDS"
 fi
 
 src_image="$src_image-debug"
 dst_image="$dst_image-debug"
 
 echo && echo "Pushing $dst_image"
-retry 2 skopeo copy --dest-creds="$DEST_CREDS" "docker://$src_image" "docker://$dst_image"
+retry 2 skopeo copy "docker://$src_image" "docker://$dst_image" --dest-authfile="$dst_authfile"
 
 src_image="${src_image%-debug}"
 dst_image="${dst_image%-debug}"
 
 echo && echo "Pushing $dst_image"
-retry 2 skopeo copy --dest-creds="$DEST_CREDS" "docker://$src_image" "docker://$dst_image"
+retry 2 skopeo copy "docker://$src_image" "docker://$dst_image" --dest-authfile="$dst_authfile"
 
 echo && echo 'Done'
