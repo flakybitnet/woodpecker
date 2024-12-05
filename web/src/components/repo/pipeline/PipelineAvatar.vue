90480c6c0725a3aef8393e8a343a872120992ef8
0x1def <0x1def@flakybit.net>
2024-12-05 14:02:38 +0300

    Reworked pipeline list view
    - split head commit into groups
    - using separate PipelineAvatar
    - moved pipeline event before message
    - always display branch or tag
    - fixed indentations
    - disabled icons shrinking
    - updated columns template
    - using dash in durations
    - added event prefix
    - using pipeline event icon

---
+++ web/src/components/repo/pipeline/PipelineAvatar.vue
@@ -0,0 +1,22 @@
+<template>
+  <SvgIcon v-if="pipeline.event === 'cron'" :path="mdiAlarm" :size="size || defaultSize" title="Cron" />
+  <img v-else class="rounded-md" :class="`w-${Math.floor(+(size || defaultSize) / 4)}`"
+       :src="pipeline.author_avatar" :title="pipeline.author" :alt="`${pipeline.author} avatar`"
+  />
+</template>
+
+<script lang="ts" setup>
+import { mdiAlarm } from '@mdi/js';
+
+import SvgIcon from '../../atomic/SvgIcon.vue';
+import type { Pipeline } from '~/lib/api/types';
+
+
+defineProps<{
+  pipeline: Pipeline;
+  size?: number | string;
+}>();
+
+const defaultSize = 32;
+
+</script>
