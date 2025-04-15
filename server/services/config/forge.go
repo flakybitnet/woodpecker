abee8f6c56b3d93e750e2b3d45aff071beb57cec
0x1def <0x1def@flakybit.net>
2025-04-15 21:14:04 +0300

    Fixed license and readme

--- server/services/config/forge.go
+++ server/services/config/forge.go
@@ -1,6 +1,6 @@
 /*
 This file is part of Woodpecker CI.
-Copyright (c) 2024 Woodpecker Authors
+Copyright (c) 2025 Woodpecker Authors
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
