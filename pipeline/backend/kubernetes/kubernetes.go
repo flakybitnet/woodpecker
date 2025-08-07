c0fd70298a9e92dd67b060ff4af7aeea31fba91b
0x1def <0x1def@flakybit.net>
2025-08-07 18:51:20 +0300

    Added user namespaces

--- pipeline/backend/kubernetes/kubernetes.go
+++ pipeline/backend/kubernetes/kubernetes.go
@@ -1,16 +1,32 @@
-// Copyright 2022 Woodpecker Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+This file is part of Woodpecker CI.
+Copyright (c) 2025 Woodpecker Authors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+This file incorporates work covered by the following copyright and permission notice:
+	Copyright (c) 2022 Woodpecker Authors
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
 
 package kubernetes
 
@@ -67,6 +83,7 @@ type config struct {
 	PodNodeSelector                map[string]string
 	ImagePullSecretNames           []string
 	PodUserHome                    string
+	PodUserNamespaces              bool
 	SecurityContext                SecurityContextConfig
 	NativeSecretsAllowFromStep     bool
 	PssProfile                     PssProfile
@@ -111,6 +128,7 @@ func configFromCliContext(ctx context.Context) (*config, error) {
 				PodNodeSelector:             make(map[string]string), // just init empty map to prevent nil panic
 				ImagePullSecretNames:        c.StringSlice("backend-k8s-pod-image-pull-secret-names"),
 				PodUserHome:                 c.String("backend-k8s-pod-user-home"),
+				PodUserNamespaces:           c.Bool("backend-k8s-pod-user-namespaces"),
 				PssProfile:                  PssProfile(c.String("backend-k8s-pss-profile")),
 				SecurityContext: SecurityContextConfig{
 					RunAsNonRoot: c.Bool("backend-k8s-secctx-nonroot"), // cspell:words secctx nonroot
